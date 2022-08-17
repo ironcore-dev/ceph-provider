9e4fef439410eea0e8cd9cf0fb9cc00485ebc083
Andreas Fritzler <andreas.fritzler@sap.com>
2022-08-17 13:32:24 +0200

    Fix linting issues

--- main.go
+++ main.go
@@ -18,22 +18,19 @@ package main
 
 import (
 	goflag "flag"
+	"os"
+
+	"github.com/onmetal/cephlet/controllers"
 	storagev1alpha1 "github.com/onmetal/onmetal-api/apis/storage"
 	rookv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
 	flag "github.com/spf13/pflag"
-	"os"
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	"github.com/onmetal/cephlet/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
