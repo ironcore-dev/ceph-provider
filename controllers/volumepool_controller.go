0a8913d32f30986c9564a92fd0b0437e2750d9b5
Lukas Frank <lukas.frank@sap.com>
2022-08-22 11:33:15 +0200

    - review comments

--- controllers/volumepool_controller.go
+++ controllers/volumepool_controller.go
@@ -110,38 +110,7 @@ func (r *VolumePoolReconciler) reconcile(ctx context.Context, log logr.Logger, p
 		return ctrl.Result{}, fmt.Errorf("failed to apply ceph volume pool %s: %w", client.ObjectKeyFromObject(rookPool), err)
 	}
 
-	availableVolumeClasses, err := r.gatherVolumeClasses(ctx)
-	if err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to gather available volume classes for volume pool: %w", err)
-	}
-
-	poolBase := pool.DeepCopy()
-	poolBase.Status.State = storagev1alpha1.VolumePoolStateAvailable
-	poolBase.Status.AvailableVolumeClasses = availableVolumeClasses
-	if err := r.Status().Patch(ctx, pool, client.MergeFrom(poolBase)); err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to patch status for volume pool: %w", err)
-	}
-
-	if err := r.Get(ctx, types.NamespacedName{Name: pool.Name, Namespace: r.RookConfig.Namespace}, rookPool); err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to fetch rook pool: %w", err)
-	}
-
-	poolBase = pool.DeepCopy()
-	switch {
-	case rookPool.Status == nil:
-		pool.Status.State = storagev1alpha1.VolumePoolStatePending
-	case rookPool.Status.Phase == rookv1.ConditionReady:
-		pool.Status.State = storagev1alpha1.VolumePoolStateAvailable
-	case rookPool.Status.Phase == rookv1.ConditionFailure:
-		pool.Status.State = storagev1alpha1.VolumePoolStateNotAvailable
-	default:
-		pool.Status.State = storagev1alpha1.VolumePoolStatePending
-	}
-	if err := r.Status().Patch(ctx, pool, client.MergeFrom(poolBase)); err != nil {
-		return ctrl.Result{}, fmt.Errorf("failed to patch status for volume pool: %w", err)
-	}
-
-	return ctrl.Result{}, nil
+	return r.patchVolumePoolStatus(ctx, pool)
 }
 
 func (r *VolumePoolReconciler) delete(ctx context.Context, log logr.Logger, pool *storagev1alpha1.VolumePool) (ctrl.Result, error) {
@@ -185,6 +154,38 @@ func (r *VolumePoolReconciler) gatherVolumeClasses(ctx context.Context) ([]corev
 		return availableVolumeClasses[i].Name < availableVolumeClasses[j].Name
 	})
 	return availableVolumeClasses, nil
+}
+
+func (r *VolumePoolReconciler) patchVolumePoolStatus(ctx context.Context, pool *storagev1alpha1.VolumePool) (ctrl.Result, error) {
+	availableVolumeClasses, err := r.gatherVolumeClasses(ctx)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to gather available volume classes for volume pool: %w", err)
+	}
+
+	rookPool := &rookv1.CephBlockPool{}
+	if err := r.Get(ctx, types.NamespacedName{Name: pool.Name, Namespace: r.RookConfig.Namespace}, rookPool); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to fetch rook pool: %w", err)
+	}
+
+	poolBase := pool.DeepCopy()
+	switch {
+	case rookPool.Status == nil:
+		pool.Status.State = storagev1alpha1.VolumePoolStatePending
+	case rookPool.Status.Phase == rookv1.ConditionReady:
+		pool.Status.State = storagev1alpha1.VolumePoolStateAvailable
+	case rookPool.Status.Phase == rookv1.ConditionFailure:
+		pool.Status.State = storagev1alpha1.VolumePoolStateNotAvailable
+	default:
+		pool.Status.State = storagev1alpha1.VolumePoolStatePending
+	}
+
+	pool.Status.AvailableVolumeClasses = availableVolumeClasses
+
+	if err := r.Status().Patch(ctx, pool, client.MergeFrom(poolBase)); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to patch status for volume pool: %w", err)
+	}
+
+	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
