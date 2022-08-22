844bde0285b3b737b18841fb65eb0bb8b5288e5a
Andreas Fritzler <andreas.fritzler@sap.com>
2022-08-22 17:53:09 +0200

    Fix tests

--- controllers/volume_controller.go
+++ controllers/volume_controller.go
@@ -247,14 +247,12 @@ func (r *VolumeReconciler) applyCephClient(ctx context.Context, log logr.Logger,
 
 	//ToDo this should be async: wait until ready and put a new request in the queue
 	if cephClient.Status == nil || cephClient.Status.Phase != rookv1.ConditionReady {
-		return "", nil
-		//		return "", fmt.Errorf("ceph client %s is not ready yet", client.ObjectKeyFromObject(cephClient))
+		return "", fmt.Errorf("ceph client %s is not ready yet", client.ObjectKeyFromObject(cephClient))
 	}
 
 	secretName, found := cephClient.Status.Info["secretName"]
 	if !found {
-		return "", nil
-		//return "", fmt.Errorf("failed to get secret name from ceph client %s status: %w", client.ObjectKeyFromObject(cephClient), err)
+		return "", fmt.Errorf("failed to get secret name from ceph client %s status: %w", client.ObjectKeyFromObject(cephClient), err)
 	}
 
 	return secretName, nil
@@ -262,11 +260,6 @@ func (r *VolumeReconciler) applyCephClient(ctx context.Context, log logr.Logger,
 
 func (r *VolumeReconciler) applySecretAndUpdateVolumeStatus(ctx context.Context, log logr.Logger, volume *storagev1alpha1.Volume, secretName string, pvc *corev1.PersistentVolumeClaim) error {
 	defer log.V(2).Info("applySecretAndUpdateVolumeStatus done")
-
-	if secretName == "" {
-		return nil
-	}
-
 	cephClientSecret := &corev1.Secret{}
 	if err := r.Get(ctx, types.NamespacedName{Namespace: r.RookConfig.Namespace, Name: secretName}, cephClientSecret); err != nil {
 		return fmt.Errorf("unable to get secret: %w", err)
@@ -392,8 +385,7 @@ func (r *VolumeReconciler) applyStorageClass(ctx context.Context, log logr.Logge
 
 	//ToDo this should be async: wait until ready and put a new request in the queue
 	if cephNs.Status == nil || cephNs.Status.Phase != rookv1.ConditionReady {
-		return "", nil
-		//return "", fmt.Errorf("empty status found for cephNS %s for volume %s", client.ObjectKeyFromObject(cephNs), client.ObjectKeyFromObject(volume))
+		return "", fmt.Errorf("empty status found for cephNS %s for volume %s", client.ObjectKeyFromObject(cephNs), client.ObjectKeyFromObject(volume))
 	}
 
 	clusterID, found := cephNs.Status.Info["clusterID"]
