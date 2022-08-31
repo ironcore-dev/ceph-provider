77521e167f5b6bd8f52f5f206e147da63e5852b4
Andreas Fritzler <andreas.fritzler@sap.com>
2022-08-31 21:10:00 +0200

    Add missing RBAC (#32)
    - Fix missing RBAC for image populator controller
    - Fix missing RBAC for volume controller

--- controllers/volume_controller.go
+++ controllers/volume_controller.go
@@ -70,6 +70,17 @@ type VolumeReconciler struct {
 //+kubebuilder:rbac:groups=ceph.rook.io,resources=cephblockpoolradosnamespaces,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ceph.rook.io,resources=cephclients,verbs=get;list;watch;create;update;patch;delete
 
+//+kubebuilder:rbac:groups=,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=,resources=persistentvolumeclaims/status,verbs=get
+//+kubebuilder:rbac:groups=,resources=persistentvolumes,verbs=get;list;watch;delete
+//+kubebuilder:rbac:groups=,resources=persistentvolumes/status,verbs=get
+
+//+kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch;create;update;patch;delete
+
+//+kubebuilder:rbac:groups=,resources=configmaps,verbs=get;list;watch
+//+kubebuilder:rbac:groups=,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=,resources=namespaces,verbs=get;list;watch
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 func (r *VolumeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
