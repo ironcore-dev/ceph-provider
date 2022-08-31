6bd54facc83321fddd48ca157846d9e9bee0c2f4
lukasfrank <lukasfrank@users.noreply.github.com>
2022-08-31 21:40:12 +0200

    Fixed rbac group (#33)

--- controllers/volume_controller.go
+++ controllers/volume_controller.go
@@ -70,16 +70,16 @@ type VolumeReconciler struct {
 //+kubebuilder:rbac:groups=ceph.rook.io,resources=cephblockpoolradosnamespaces,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ceph.rook.io,resources=cephclients,verbs=get;list;watch;create;update;patch;delete
 
-//+kubebuilder:rbac:groups=,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=,resources=persistentvolumeclaims/status,verbs=get
-//+kubebuilder:rbac:groups=,resources=persistentvolumes,verbs=get;list;watch;delete
-//+kubebuilder:rbac:groups=,resources=persistentvolumes/status,verbs=get
+//+kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=persistentvolumeclaims/status,verbs=get
+//+kubebuilder:rbac:groups=core,resources=persistentvolumes,verbs=get;list;watch;delete
+//+kubebuilder:rbac:groups=core,resources=persistentvolumes/status,verbs=get
 
 //+kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch;create;update;patch;delete
 
-//+kubebuilder:rbac:groups=,resources=configmaps,verbs=get;list;watch
-//+kubebuilder:rbac:groups=,resources=secrets,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=,resources=namespaces,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
