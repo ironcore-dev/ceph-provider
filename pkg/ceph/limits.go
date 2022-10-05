477af9fa970d7eae09fc5a75cafc05e492c54d6d
Lukas Frank <lukas.frank@sap.com>
2022-10-05 15:39:46 +0200

    tests

--- pkg/ceph/limits.go
+++ pkg/ceph/limits.go
@@ -1,7 +1,22 @@
+// Copyright 2022 OnMetal authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ceph
 
 import (
 	"fmt"
+
 	"github.com/onmetal/onmetal-api/apis/storage"
 	"k8s.io/apimachinery/pkg/api/resource"
 )
@@ -24,7 +39,7 @@ func CalculateLimits(volume *storage.Volume, volumeClass *storage.VolumeClass) (
 	if _, limitsPerGB := volumeClass.Labels[LabelLimitsPerGB]; limitsPerGB {
 		size := volume.Spec.Resources.Storage()
 		if size == nil {
-			return nil, fmt.Errorf("")
+			return nil, fmt.Errorf("unable to calculate scale factor because storage size is not set")
 		}
 
 		scale = size.ScaledValue(resource.Giga)
