0c1ee145a40dd771a6d116a802bfa6af7b5af877
Lukas Frank <lukas.frank@sap.com>
2022-11-02 15:35:37 +0100

    fix lint issues

--- main.go
+++ main.go
@@ -18,14 +18,13 @@ package main
 
 import (
 	goflag "flag"
-	"github.com/prometheus/client_golang/prometheus"
 	"os"
-	"sigs.k8s.io/controller-runtime/pkg/metrics"
 
 	snapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
 	"github.com/onmetal/cephlet/controllers"
 	"github.com/onmetal/cephlet/pkg/rook"
 	storagev1alpha1 "github.com/onmetal/onmetal-api/apis/storage/v1alpha1"
+	"github.com/prometheus/client_golang/prometheus"
 	rookv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
 	flag "github.com/spf13/pflag"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -35,6 +34,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 	//+kubebuilder:scaffold:imports
 )
 
