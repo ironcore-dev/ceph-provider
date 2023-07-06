a2c9538e7e0a6b04a5f5eb53f4187728d480179f
Pradumna Panditrao <pradumna.panditrao@t-systems.com>
2023-07-06 16:08:06 +0530

    Added 1st version of  unit test for Create, Get, delete Co-authored-by: aditya-dixit99 <aditya.dixit@t-systems.com>

--- e2e/volume_test.go
+++ e2e/volume_test.go
@@ -1,47 +1,100 @@
 package e2e
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	//. "github.com/onsi/gomega"
+	//"github.com/onmetal/onmetal-api/testutils"
+	//"flag"
 	"fmt"
-	"flag"
-	"context"
-	 metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	//corev1 "k8s.io/api/core/v1"
+
+	. "github.com/onsi/ginkgo/v2"
+
+	//. "github.com/onsi/gomega"
+
+	corev1alpha1 "github.com/onmetal/onmetal-api/api/core/v1alpha1"
+	storagev1alpha1 "github.com/onmetal/onmetal-api/api/storage/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	//"k8s.io/apimachinery/pkg/types
+
+	//testutils "github.com/onmetal/onmetal-api/utils/testing"
+
+	//. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	//. "sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 )
 
 var _ = Describe("cephlet-volume", func() {
-	/*It("Should pass", func() {
-	  fmt.Println("Gingo working...........")
-           Expect(1).To(Equal(1))
-       })*/
-	It("should create a basic volume", func(ctx SpecContext) {
 
-		//todo
-		//podList := corev1.PodList{}
-		
-		kubeconfig := flag.String("kubeconfig", "/home/ppanditrao/.kube/config", "location to your kubecofig file")
-		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err !=nil{
-			//handle error
-		}
-		//fmt.Println(config)
-		clientset, err :=kubernetes.NewForConfig(config)
-		if err !=nil{
-			//handle error
-		}
+	// var (
+	// 	volumeClass *storagev1alpha1.VolumeClass
+	// 	volumePool  *storagev1alpha1.VolumePool
+	// )
 
-		pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
-		if err !=nil{
-			//handle error
+	const (
+		volumeSize = "1Gi"
+	//	snapshotSize  = "2Gi"
+	//	cephPoolName  = "ceph"
+	//	cephImageName = "image-1"
+	)
+
+	It("should create volume", func(ctx SpecContext) {
+		By("checking that a Volume has been created")
+		vol := &storagev1alpha1.Volume{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "tsi",
+				//GenerateName: "volume-",
+				Namespace: "rook-ceph",
+			},
+			Spec: storagev1alpha1.VolumeSpec{
+				VolumeClassRef: &corev1.LocalObjectReference{Name: "fast"},
+				VolumePoolRef:  &corev1.LocalObjectReference{Name: "ceph"},
+				Resources: corev1alpha1.ResourceList{
+					corev1alpha1.ResourceStorage: resource.MustParse(volumeSize),
+				},
+			},
 		}
-		fmt.Println(pods)
-		
-		//Expect(k8sClient.List(ctx, &podList)).To(Succeed())
-		
+		Expect(k8sClient.Create(ctx, vol)).To(Succeed())
+	})
+
+	It("Should get the volume", func(ctx SpecContext) {
+		fmt.Println("Gingo working...........")
+
+		volume := &storagev1alpha1.Volume{}
+		ns := types.NamespacedName{Namespace: "rook-ceph", Name: "tsi"}
+		Expect(k8sClient.Get(ctx, ns, volume)).To(Succeed())
+		fmt.Println(volume.Name)
+
+		// Todo use matcher
+		//Expect(volume.Name).To(Succeed())
+
+		//Expect(k8sClient.List(ctx, volumeList, client.InNamespace("rook-ceph"))).To(Succeed())
+	})
+
+	It("Should delete volume", func(ctx SpecContext) {
+		fmt.Println("Gingo working...........")
+
+		volume := &storagev1alpha1.Volume{}
+		//volumeList := &storagev1alpha1.vol
+		ns := types.NamespacedName{Namespace: "rook-ceph", Name: "tsi"}
+		err := k8sClient.Get(ctx, ns, volume)
+		if err != nil {
+
+		}
+		deleteResult := k8sClient.Delete(ctx, volume)
+		fmt.Println(deleteResult)
+		Expect(deleteResult).To(Succeed())
+		fmt.Println(volume.Name)
+		// Expect(deleteResult).Should(compareDeleteResult)
+
+		// func () compareDeleteResult(a error) {
+		// 	if a != nil {
+		// 		return false
+		// 	 }
+		// 	 return true
+		// }
 
 	})
-})
 
+})
