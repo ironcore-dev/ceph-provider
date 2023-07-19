d99fd7885e1cd3f64dbaed1a0d59ab4b734b3a95
Pradumna Panditrao <pradumna.panditrao@t-systems.com>
2023-07-19 04:56:10 +0000

    Added ceph module and tried to run the testcase with RBD connection:  --ceph-pool=ceph --ceph-user=volume-rook-ceph--ceph --ceph-client=volume-rook-ceph--ceph --ceph-mornitors=--ceph-keyringfile=keyring-only-ceph --ceph-keyfile=kek : WIP
    Co-authored-by: aditya-dixit99 <aditya.dixit@t-systems.com>

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -16,15 +16,16 @@ package e2e
 
 import (
 	"flag"
-	"fmt"
 	"testing"
 	"time"
+	"fmt"
 
 	"github.com/ceph/go-ceph/rados"
 	"github.com/onmetal/controller-utils/configutils"
 	rookv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/onmetal/cephlet/pkg/ceph"
 	storagev1alpha1 "github.com/onmetal/onmetal-api/api/storage/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -39,6 +40,7 @@ import (
 var (
 	cfg       *rest.Config
 	k8sClient client.Client
+	opts      Options
 )
 
 var (
@@ -55,24 +57,33 @@ const (
 	consistentlyDuration = 1 * time.Second
 )
 
+type Options struct {
+	Address string
+
+	PathSupportedVolumeClasses string
+
+	Ceph CephOptions
+}
 
 type CephOptions struct {
 	Monitors    string
 	User        string
-	KeyFile     string
 	KeyringFile string
+	KeyFile     string
 	Pool        string
 	Client      string
+	KeyEncryptionKeyPath string
 }
 
 // Register your flags in an init function.  This ensures they are registered _before_ `go test` calls flag.Parse().
 func init() {
-  flag.StringVar(&cephOptions.Pool,"ceph-pool","","ceph pool")
-  flag.StringVar(&cephOptions.User,"ceph-user","","ceph user")
-  flag.StringVar(&cephOptions.Client,"ceph-client","","ceph client")
-  flag.StringVar(&cephOptions.KeyFile,"ceph-keyfile","","ceph keyfile")
-  flag.StringVar(&cephOptions.KeyringFile,"ceph-keyringfile","","ceph-keyring file")
-  flag.StringVar(&cephOptions.Monitors,"ceph-mornitors","","ceph monitors")
+	flag.StringVar(&cephOptions.Pool, "ceph-pool", "", "ceph pool")
+	flag.StringVar(&cephOptions.User, "ceph-user", "", "ceph user")
+	flag.StringVar(&cephOptions.Client, "ceph-client", "", "ceph client")
+	flag.StringVar(&cephOptions.KeyringFile, "ceph-keyringfile", "", "ceph-keyring file")
+	flag.StringVar(&cephOptions.KeyFile, "ceph-keyfile", "", "ceph keyfile")
+	flag.StringVar(&cephOptions.Monitors, "ceph-mornitors", "", "ceph monitors")
+	flag.StringVar(&cephOptions.KeyEncryptionKeyPath, "ceph-kek-path", "", "path to the key encryption key file (32 Bit - KEK) to encrypt volume keys.")
 }
 
 func TestControllers(t *testing.T) {
@@ -103,8 +114,7 @@ var _ = BeforeSuite(func(ctx SpecContext) {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 	SetClient(k8sClient)
-	
-	
+
 	cephCluster = &rookv1.CephCluster{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "my-cluster",
@@ -124,29 +134,34 @@ var _ = BeforeSuite(func(ctx SpecContext) {
 	Eventually(Object(cephPool)).Should(SatisfyAll(
 		HaveField("Status.Phase", rookv1.ConditionReady),
 	))
-        
+
 	cephClient = &rookv1.CephClient{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      cephOptions.Client,
+			//Name: "volume-rook-ceph--ceph",
 			Namespace: "rook-ceph",
 		},
 	}
+	fmt.Println("cephclient",cephOptions.Client)
 	Eventually(Object(cephClient)).Should(SatisfyAll(
 		HaveField("Status.Phase", rookv1.ConditionReady),
 	))
 
-	
 	conn, err := ceph.ConnectToRados(ctx, ceph.Credentials{
-	      Monitors: cephOptions.Monitors,
-	      User:     cephOptions.User,
-	      Keyfile:  cephOptions.KeyFile,
+		Monitors: cephOptions.Monitors,
+		User:     cephOptions.User,
+		Keyfile:  cephOptions.KeyFile,
 	})
 
 	Expect(err).NotTo(HaveOccurred())
 
-	//err = ceph.CheckIfPoolExists(conn, opts.Ceph.Pool)
-	//Expect(err).NotTo(HaveOccurred())
-	
+	if err != nil {
+		return
+		//return fmt.Errorf("failed to establish rados connection: %w", err)
+	}
+
+	if err := ceph.CheckIfPoolExists(conn, opts.Ceph.Pool); err != nil {
+		return
+	}
 
 })
-
