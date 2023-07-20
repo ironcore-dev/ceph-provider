dad4ba131ff0f62f30a98a157105abe7662b97fd
Pradumna Panditrao <pradumna.panditrao@t-systems.com>
2023-07-20 11:43:55 +0000

    Added metadata validation
    Co-authored-by: aditya-dixit99 <aditya.dixit@t-systems.com>

--- e2e/volume_test.go
+++ e2e/volume_test.go
@@ -16,7 +16,10 @@ package e2e
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
 	librbd "github.com/ceph/go-ceph/rbd"
 	corev1alpha1 "github.com/onmetal/onmetal-api/api/core/v1alpha1"
 	storagev1alpha1 "github.com/onmetal/onmetal-api/api/storage/v1alpha1"
@@ -26,33 +29,31 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"strings"
 )
 
 var _ = Describe("cephlet-volume", func() {
-	
-	/*var (
-	 	volumeClass *storagev1alpha1.VolumeClass
-	 	//volumePool  *storagev1alpha1.VolumePool
-		//rookConfig                 *rook.Config
-		//volumePoolSecretAnnotation = "ceph-client-secret-name"
-	 )*/
 
+	// var (
+	// //volumeClass *storagev1alpha1.VolumeClass
+	// //volumePool  *storagev1alpha1.VolumePool
+	// //rookConfig                 *rook.Config
+	// //volumePoolSecretAnnotation = "ceph-client-secret-name"
+	// )
 
 	const (
 		volumeSize = "10Gi"
 
 		//TODO get from volumeclass
-		fastVolClassTPS = 100
-		fastVolClassIPOS = 100
-		fastVolClassBurstFactor = 3
-		fastVolClassBurstDurationInSec = 10
+		fastVolClassTPS                 = 100
+		fastVolClassIOPS                = 100
+		fastVolClassBurstFactor         = 1
+		fastVolClassBurstDurationInSec  = 10
+
 	//	cephClientSecretValue = "test"
 	//	snapshotSize  = "2Gi"
 	//	cephPoolName  = "ceph"
 	//	cephImageName = "image-1"
 	)
-	
 
 	It("should create volume", func(ctx SpecContext) {
 		By("checking that a Volume has been created")
@@ -78,46 +79,67 @@ var _ = Describe("cephlet-volume", func() {
 		volume := &storagev1alpha1.Volume{}
 		ns := types.NamespacedName{Namespace: "rook-ceph", Name: "tsi"}
 		Expect(k8sClient.Get(ctx, ns, volume)).To(Succeed())
-		if (volume.Status.State == "Available"){
+		if volume.Status.State == "Available" {
 			imageName := strings.Split(volume.Status.Access.VolumeAttributes["image"], "/")[1]
-		
 
 			fmt.Println("RBD Image details", imageName)
 			img1, err := librbd.OpenImage(ioCtx, imageName, librbd.NoSnapshot)
 			if err != nil {
 				fmt.Println("Errrorrrrrrrr", err)
-				
+
 			}
 			metaData, err := img1.ListMetadata()
 			fmt.Println("Metadata of image is::::::::::::::: :", metaData)
 			Expect(err).NotTo(HaveOccurred())
-			fmt.Println("Volume Image for TSI::::::::::::",imageName)
+			fmt.Println("Volume Image for TSI::::::::::::", imageName)
 
-                       iopsBurstLimit := fastVolClassBurstfactor * fastVolClassIOPS
-                         
-   		       setIOPSBurstLimit, err := strconv.Atoi(metaData["rbd_qos_iops_limit"])
-                       if err != nil {
-                          // ... handle error
-                           panic(err)
+			// TPS/BPS Data
+			tpsBurstLimit := fastVolClassBurstFactor * fastVolClassTPS
+			tpsBurst := tpsBurstLimit * fastVolClassBurstDurationInSec
 
-                       }
-		       fmt.Println("set isssssssssssssss",setIOPSBurstLimit)
-		       fmt.Println("expected issssssssssssssssss",iopsBurstLimit)
-		       /*
-		       limits[rbd_qos_iops_burst] = iopsBurstLimit
-	limits[rbd_qos_read_iops_burst] = iopsBurstLimit
-	limits[rbd_qos_write_iops_burst] = iopsBurstLimit
-		       */
-                     
-		}else {
+			setTPSBurstLimit, err := strconv.Atoi(metaData["conf_rbd_qos_bps_limit"])
+			if err != nil {
+				panic(err)
+			}
+			setTPSBurst,err := strconv.Atoi(metaData["conf_rbd_qos_read_bps_burst"])
+			if err != nil {
+				panic(err)
+			}
+			// IOPS Data			
+			iopsBurstLimit := fastVolClassBurstFactor * fastVolClassIOPS
+			iopsBurst := iopsBurstLimit * fastVolClassBurstDurationInSec
+
+			setIOPSBurst, err := strconv.Atoi(metaData["conf_rbd_qos_iops_burst"])
+			if err != nil {
+				panic(err)
+			}
+
+			setIOPSBurstLimit, err := strconv.Atoi(metaData["conf_rbd_qos_iops_limit"])
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println("set isssssssssssssss", setTPSBurst)
+			fmt.Println("expected issssssssssssssssss",tpsBurst)
+			
+			Expect(setIOPSBurstLimit).To(Equal(iopsBurstLimit))
+			Expect(setTPSBurstLimit).To(Equal(tpsBurstLimit))
+			Expect(setTPSBurst).To(Equal(tpsBurst))
+			Expect(setIOPSBurst).To(Equal(iopsBurst))
+
+			/*
+				limits[rbd_qos_iops_burst] = iopsBurstLimit
+				limits[rbd_qos_read_iops_burst] = iopsBurstLimit
+				limits[rbd_qos_write_iops_burst] = iopsBurstLimit
+			*/
+
+		} else {
 			fmt.Println("Though the volume created, image is not created because Cephlet and Volumepoollet is not running.")
 		}
-		
 
 		// Eventually(Object(volume)).Should(SatisfyAll(
 		// 	HaveField(volume.Status.State, "Available"),
 		// ))
-		
 
 		fmt.Println("Here the Volume is getting created############", vol.Name)
 	})
@@ -127,40 +149,33 @@ var _ = Describe("cephlet-volume", func() {
 		ns := types.NamespacedName{Namespace: "rook-ceph", Name: "tsi"}
 		Expect(k8sClient.Get(ctx, ns, volume)).To(Succeed())
 		fmt.Println("Here the Volume is getting listed##############", volume.Name)
-		
-		
+
 		// Todo use matcher
 		//Expect(volume.Name).To(Succeed())
-
 		//Expect(k8sClient.List(ctx, volumeList, client.InNamespace("rook-ceph"))).To(Succeed())
 	})
 
-	// It("Should delete volume", func(ctx SpecContext) {
+	It("Should delete volume", func(ctx SpecContext) {
 
-	// 	volume := &storagev1alpha1.Volume{}
-	// 	//volumeList := &storagev1alpha1.vol
-	// 	ns := types.NamespacedName{Namespace: "rook-ceph", Name: "tsi"}
-	// 	err := k8sClient.Get(ctx, ns, volume)
-	// 	if err != nil {
+		volume := &storagev1alpha1.Volume{}
+		//volumeList := &storagev1alpha1.vol
+		ns := types.NamespacedName{Namespace: "rook-ceph", Name: "tsi"}
+		err := k8sClient.Get(ctx, ns, volume)
+		if err != nil {
 
-	// 	}
-	// 	deleteResult := k8sClient.Delete(ctx, volume)
-	// 	//fmt.Println(deleteResult)
-	// 	Expect(deleteResult).To(Succeed())
-	// 	fmt.Println("Here the Volume is getting deleted which was ealier created.###########", volume.Name)
+		}
+		deleteResult := k8sClient.Delete(ctx, volume)
+		//fmt.Println(deleteResult)
+		Expect(deleteResult).To(Succeed())
+		fmt.Println("Here the Volume is getting deleted which was ealier created.###########", volume.Name)
 
-	// 	Expect(k8sClient.Get(ctx, ns, volume)).To(Succeed())
-	// 	if (volume.Status.State != ""){
-	// 		fmt.Println("Image is deleted completely which is", volume.Status.Access.VolumeAttributes["image"])
-	// 	}else {
-	// 		fmt.Println("Image/Volume not deleted completely, it is there")
-	// 	}
+		Expect(k8sClient.Get(ctx, ns, volume)).To(Succeed())
+		if (volume.Status.State != ""){
+			fmt.Println("Image is deleted completely which is", volume.Status.Access.VolumeAttributes["image"])
+		}else {
+			fmt.Println("Image/Volume not deleted completely, it is there")
+		}
 
-	// })
+	})
 
 })
-// var ImageRBDIDPrefix    
-// = "img_"
-// func ImageIDToRBDID(imageID string) string {
-// 	return ImageRBDIDPrefix + imageID
-// }
