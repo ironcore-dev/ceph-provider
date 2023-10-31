1afa1969bcdd8c7cf24a57edb62b62095f250294
ushabelgur <usha.belgur26@gmail.com>
2023-10-31 21:40:02 +0530

    test case changes to remove  api dependency from bucket class

--- ori/bucket/server/bucketclass_list_test.go
+++ ori/bucket/server/bucketclass_list_test.go
@@ -15,43 +15,14 @@
 package server_test
 
 import (
-	corev1alpha1 "github.com/onmetal/onmetal-api/api/core/v1alpha1"
-	storagev1alpha1 "github.com/onmetal/onmetal-api/api/storage/v1alpha1"
-	oriv1alpha1 "github.com/onmetal/onmetal-api/ori/apis/bucket/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	oriv1alpha1 "github.com/onmetal/onmetal-api/ori/apis/bucket/v1alpha1"
 )
 
 var _ = Describe("ListBucketClasses test", func() {
 	It("Should check empty BucketClasses list", func(ctx SpecContext) {
-		By("Creating a BucketClass")
-		bucketClass1 := &storagev1alpha1.BucketClass{
-			TypeMeta: metav1.TypeMeta{},
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "bar",
-			},
-			Capabilities: corev1alpha1.ResourceList{
-				corev1alpha1.ResourceIOPS: resource.MustParse("200"),
-				corev1alpha1.ResourceTPS:  resource.MustParse("2"),
-			},
-		}
-		Expect(k8sClient.Create(ctx, bucketClass1)).To(Succeed())
-
-		By("Creating a second BucketClass")
-		bucketClass2 := &storagev1alpha1.BucketClass{
-			TypeMeta: metav1.TypeMeta{},
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "foo",
-			},
-			Capabilities: corev1alpha1.ResourceList{
-				corev1alpha1.ResourceIOPS: resource.MustParse("100"),
-				corev1alpha1.ResourceTPS:  resource.MustParse("1"),
-			},
-		}
-		Expect(k8sClient.Create(ctx, bucketClass2)).To(Succeed())
-
 		By("Listing the available BucketClasses")
 		listBuckClasses, err := bucketClient.ListBucketClasses(ctx, &oriv1alpha1.ListBucketClassesRequest{})
 		Expect(err).NotTo(HaveOccurred())
@@ -72,16 +43,5 @@ var _ = Describe("ListBucketClasses test", func() {
 				},
 			},
 		))
-
-		By("Deleting the BucketClasses")
-		Expect(k8sClient.Delete(ctx, bucketClass1)).To(Succeed())
-		Expect(k8sClient.Delete(ctx, bucketClass2)).To(Succeed())
-
-		By("Ensuring that the BucketClasses list is empty")
-		Eventually(ctx, func() {
-			listBuckClasses, err = bucketClient.ListBucketClasses(ctx, &oriv1alpha1.ListBucketClassesRequest{})
-			Expect(err).NotTo(HaveOccurred())
-			Expect(listBuckClasses.BucketClasses).To(BeEmpty())
-		})
 	})
 })
---
+++ pkg/bcr/registry.go
@@ -0,0 +1,76 @@
+// Copyright 2023 OnMetal authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bcr
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"k8s.io/apimachinery/pkg/util/yaml"
+
+	ori "github.com/onmetal/onmetal-api/ori/apis/bucket/v1alpha1"
+)
+
+func LoadBucketClasses(reader io.Reader) ([]ori.BucketClass, error) {
+	var classList []ori.BucketClass
+	if err := yaml.NewYAMLOrJSONDecoder(reader, 4096).Decode(&classList); err != nil {
+		return nil, fmt.Errorf("unable to unmarshal bucket classes: %w", err)
+	}
+
+	return classList, nil
+}
+
+func LoadBucketClassesFile(filename string) ([]ori.BucketClass, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open bucket class file (%s): %w", filename, err)
+	}
+
+	return LoadBucketClasses(file)
+}
+
+func NewBucketClassRegistry(classes []ori.BucketClass) (*Bcr, error) {
+	registry := Bcr{
+		classes: map[string]ori.BucketClass{},
+	}
+
+	for _, class := range classes {
+		if _, ok := registry.classes[class.Name]; ok {
+			return nil, fmt.Errorf("multiple classes with same name (%s) found", class.Name)
+		}
+		registry.classes[class.Name] = class
+	}
+
+	return &registry, nil
+}
+
+type Bcr struct {
+	classes map[string]ori.BucketClass
+}
+
+func (v *Bcr) Get(bucketClassName string) (*ori.BucketClass, bool) {
+	class, found := v.classes[bucketClassName]
+	return &class, found
+}
+
+func (v *Bcr) List() []*ori.BucketClass {
+	var classes []*ori.BucketClass
+	for name := range v.classes {
+		class := v.classes[name]
+		classes = append(classes, &class)
+	}
+	return classes
+}
