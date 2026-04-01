0380596b3c5667ba4161c7dd3fbbe0ed65744ad4
kajol.asabe@t-systems.com <kajol.asabe@t-systems.com>
2026-04-01 16:50:21 +0530

    Enhance volume list label filter operation
    Signed-off-by: kajol.asabe@t-systems.com <kajol.asabe@t-systems.com>

--- internal/volumeserver/volume_create.go
+++ internal/volumeserver/volume_create.go
@@ -119,7 +119,8 @@ func (s *Server) createImageFromVolume(ctx context.Context, log logr.Logger, vol
 
 	image := &api.Image{
 		Metadata: apiutils.Metadata{
-			ID: s.idGen.Generate(),
+			ID:     s.idGen.Generate(),
+			Labels: volume.Metadata.Labels,
 		},
 		Spec: api.ImageSpec{
 			Size:              imageSize,
